bc675d13b32244304e2b3b5dfc068409d27d8ff5
Yoon, DeuckGi <isz03072@gmail.com>
2020-04-23 20:12:45 +0900

    E03List 실습예제

---
+++ app/src/main/java/net/skhu/MainActivity.java
@@ -0,0 +1,20 @@
+package net.skhu;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void btnListView_clicked(View view) {
+        Intent intent = new Intent(this, ListViewActivity.class);
+        startActivity(intent);
+    }
+    public void btnRecyclerView_clicked(View view) {
+        Intent intent = new Intent(this, RecyclerViewActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/net/skhu/RecyclerViewAdapter.java
@@ -0,0 +1,36 @@
+package net.skhu;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+        public ViewHolder(View view) {
+            super(view);
+            textView = view.findViewById(R.id.textView);
+        }
+    }
+    LayoutInflater layoutInflater;
+    ArrayList<String> arrayList;
+    public RecyclerViewAdapter(Context context, ArrayList<String> arrayList) {
+        this.layoutInflater = LayoutInflater.from(context);
+        this.arrayList = arrayList;
+    }
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = layoutInflater.inflate(R.layout.item1, viewGroup, false);
+        return new ViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(final ViewHolder viewHolder, final int index) {
+        viewHolder.textView.setText(arrayList.get(index));
+    }
+}
